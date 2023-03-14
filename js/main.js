36721491872784f55bc843cdc2693abf3cb9c0d9
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-03-14 14:53:17 +0100

    stampato a schermo messaggio in h1 tramite vue

---
+++ js/main.js
@@ -0,0 +1,16 @@
+/**
+ * Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+ */
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            message: 'Ciao, benvenuto nella mia pagina VUE JS'
+        }
+    }
+}).mount('#app')
